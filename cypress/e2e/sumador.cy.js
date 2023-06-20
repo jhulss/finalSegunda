51f0e9b25dd1f73fc36fddfbaa435e4782a36e23
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-20 10:40:38 -0400

    se muestra un mensaje cuando el post no tiene contenido

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -5,13 +5,14 @@ describe("Postear", () => {
     cy.get("post-button").click();
     cy.get("#resultado-div").should("contain", "Holaa como estas");
   });
-
-  it("Deberia mostrar un mensje de error al no tener contenido", () => {
+  
+  it("Deberia mostrar un mensaje de error cuando el post no tiene contenido", () => {
     cy.visit("/");
     cy.get("#post").type("");
     cy.get("post-button").click();
-    cy.get("#resultado-div").should("contain", "No es posible postear sin contenido");
+    cy.get("#resultado-div").should("No es posible postear sin contenido");
   });
+
 
 
 });
