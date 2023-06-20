a3f630529b71fe03ebddcb834e547b2f7e96566a
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-20 10:05:51 -0400

    Segunda prueba verificando que la cadena es correcta

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -3,6 +3,12 @@ import Verificardor from "./sumador";
 describe("Verificar", () => {
   it("Deberia devolver un carater", () => {
     const verifi = new Verificardor ();
-    expect(verifi.verificar(",")).toEqual(",");
+    expect(verifi.verific(",")).toEqual(",");
   });
+
+  it("Deberia devolver correcto ya que la cadena esta equilibrada", () => {
+    const verifi = new Verificardor ();
+    expect(verifi.verificar("[]")).toEqual("correcto");
+  });
+
 });
